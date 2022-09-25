960e107a7a5b7dbc688d98e8e9d43f2a217ad058
Roberto Ramirez <robert.624@hotmail.com>
2022-09-25 01:24:25 -0500

    endpoint sends html file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 let express = require('express');
 let app = express();
+const path = require('path')
 
 console.log("Hello World")
 
 app.get("/", (req, res)=>{
-    res.send('Hello Express')
+    res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
 

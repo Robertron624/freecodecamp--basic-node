14aa14c84e704bb4b0970994c94c3af82e55a69b
Roberto Ramirez <robert.624@hotmail.com>
2022-09-25 13:59:55 -0500

    added middleware chaining

--- myApp.js
+++ myApp.js
@@ -16,6 +16,17 @@ app.use((req, res, next) => {
     next()
 }) 
 
+app.get('/now', (req, res, next)=>{
+    req.time = new Date().toString()
+    next()
+},
+    (req, res)=>{
+        res.send({
+            time: req.time
+        })
+    }
+)
+
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, './views/index.html'))
 })

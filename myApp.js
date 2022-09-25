acdbd0fd5056ce52279cf215ae4e91209d6c66b3
Roberto Ramirez <robert.624@hotmail.com>
2022-09-25 01:36:06 -0500

    fix style middelware

--- myApp.js
+++ myApp.js
@@ -4,12 +4,11 @@ const path = require('path')
 
 console.log("Hello World")
 
-app.use(express.static( path.join(__dirname, './public')))
+app.use('/public',express.static( path.join(__dirname, '/public')))
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, './views/index.html'))
 })
-
 
 
 

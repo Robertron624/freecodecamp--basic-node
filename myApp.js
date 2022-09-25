24ab3e0c12ac1698e1f300721f5d22c6c12c0089
Roberto Ramirez <robert.624@hotmail.com>
2022-09-25 11:32:38 -0500

    using virtual env

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 const path = require('path')
+require('dotenv').config()
 
 console.log("Hello World")
 
@@ -10,9 +11,12 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, './views/index.html'))
 })
 
+
 app.get('/json', (req, res)=>{
+    const caseStyle = process.env.MESSAGE_STYLE
+    const message = caseStyle == 'uppercase'? 'HELLO JSON' : 'Hello json'
     res.json({
-    "message": "Hello json"
+    "message": message
     })
 })
 
